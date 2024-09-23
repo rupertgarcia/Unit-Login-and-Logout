acc78e764e47fd935bcfa88c55d7c0187eacd5eb
Rupert Garcia <rupert.garcia.ph@gmail.com>
2024-09-23 17:10:13 +0800

    Updated Logged In and Logged Out db

--- login.php
+++ login.php
@@ -12,17 +12,19 @@ if (isset($_POST['submit'])) {
     $brandUnit = $_POST['brandUnit'];
     $chargerOption = $_POST['charger'];  // This is either 'With Charger' or 'Without Charger'
     $purposeUnit = $_POST['purposeUnit'];
-    $submissionDate = date("Y-m-d H:i:s");  // Get the current date and time
+    $dateLoggedIn = date("Y-m-d H:i:s");  // Get the current date and time for logged in
+    $dateLoggedOut = NULL;  // Initialize date_logged_out as NULL for now
+    $isLoggedOut = 0;  // Set is_logged_out to 0 since logged out is NULL
 
     // Prepare the SQL insert query
-    $sql = "INSERT INTO UnitLogInForm (requestor_name, id_number, local_number, email_address, asset_tag_number, brand_unit, charger_option, purpose_of_borrowing, submission_date) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    $sql = "INSERT INTO UnitLogInForm (requestor_name, id_number, local_number, email_address, asset_tag_number, brand_unit, charger_option, purpose_of_borrowing, date_logged_in, date_logged_out, is_logged_out) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
     // Prepare the statement
     $stmt = $conn->prepare($sql);
 
     // Bind the parameters to the query
-    $stmt->bind_param("sisssssss", $requestorName, $idNumber, $localNumber, $email, $assetTag, $brandUnit, $chargerOption, $purposeUnit, $submissionDate);
+    $stmt->bind_param("sissssssssi", $requestorName, $idNumber, $localNumber, $email, $assetTag, $brandUnit, $chargerOption, $purposeUnit, $dateLoggedIn, $dateLoggedOut, $isLoggedOut);
 
     // Execute the statement
     if ($stmt->execute()) {
